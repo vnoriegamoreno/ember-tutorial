cc027cc765195ec9e66bf6d1a1989988415055d8
Vicente Noriega <vicente.noriega.sc@gmail.com>
2022-08-16 00:53:55 -0500

    Helpers

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route';
+import { products } from '../data/products';
 
 export default class ItemRoute extends Route {
   model({ item_id }) {
-    return item_id;
+    const product = products.find(({ id }) => id === item_id);
+    return product;
   }
 }
